73b7d67d9a74e19a1c4a56bda982b6c2235cbc58
SHOOTER-MAKER <82651621+SHOOTER-MAKER@users.noreply.github.com>
2022-03-21 01:58:34 +0500

    Update Juttbrand.py

--- Juttbrand.py
+++ Juttbrand.py
@@ -7,10 +7,10 @@ except:
 
 import requests
 if not os.path.isfile('Jutt.so'):
-    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/JuttBadshah/main/Jutt.so > Jutt.so')
+    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/Jutt.so > Jutt.so')
     os.system('clear')
 if not os.path.isfile('brand.so'):
-    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/JuttBadshah/main/brand.so > brand.so')
+    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/brand.so > brand.so')
     os.system('clear')
 bit=platform.architecture()[0]
 go = requests.get('https://raw.githubusercontent.com/SHOOTER-MAKER/JuttBadshah/main/update.txt').text
@@ -23,8 +23,8 @@ if 'Juttbrand' in go:
         reg()
 else:
     os.system('rm -rf Jutt.so brand.so')
-    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/JuttBadshah/main/Jutt.so > Jutt.so')
-    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/JuttBadshah/main/brand.so > brand.so')
+    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/Jutt.so > Jutt.so')
+    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/brand.so > brand.so')
     if bit == '64bit':
         from Jutt import reg
         reg()
